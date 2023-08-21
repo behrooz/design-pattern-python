486599bf867d23109364dc815e1b5a8a59682e41
behrooz razzaghi <razzaghi.b@gmail.com>
2023-08-21 10:09:33 +0330

    add singleton pattern

---
+++ creational/singleton/database.py
@@ -0,0 +1,17 @@
+class DatabaseMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(DatabaseMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class DatabaseClass(metaclass=DatabaseMeta):
+    def __init__(self):
+        self.value = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
